250373ef1eafa33dc3c85d5504d4c5d5d6e2564d
Nanne Baars <nbaars@xebia.com>
2015-04-21 08:42:39 +0200

    pom file is created

---
+++ lesson-converter/src/main/java/org/owasp/webgoat/converter/ConverterMain.java
@@ -0,0 +1,77 @@
+package org.owasp.webgoat.converter;
+
+import lombok.extern.java.Log;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.annotation.Arg;
+import net.sourceforge.argparse4j.impl.Arguments;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+
+@Log
+public class ConverterMain {
+
+    private static class Options {
+
+        @Arg(dest = "src_dir")
+        public File sourceDir;
+
+        @Arg(dest = "dest_dir")
+        public File destDir;
+
+        @Arg(dest = "lesson_name")
+        public String lessonName;
+
+        @Arg(dest = "overwrite")
+        public Boolean overwrite = false;
+    }
+
+    public static final ArgumentParser parser = ArgumentParsers.newArgumentParser("Lesson converter")
+            .description("Converts a legacy lesson to a new plugin lesson");
+
+    public ConverterMain(Options options) {
+        log.setLevel(Level.ALL);
+
+        LessonCreator lesson = new LessonCreator(options.lessonName, options.destDir.toPath());
+        try {
+            if (options.overwrite) {
+                lesson.deleteDirectory();
+            }
+            lesson.createDirectory();
+            lesson.writePomFile();
+            lesson.copyJavaSourceFiles();
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "Unable to convert", e);
+        }
+    }
+
+    /**
+     * Start the conversion, accepted command line options:
+     *
+     * @param args
+     */
+    public static void main(String[] args) throws ArgumentParserException {
+        parser.addArgument("-s", "--src-dir") //
+                .help("Top level directory of the old WebGoat legacy project") //
+                .type(Arguments.fileType().verifyIsDirectory().verifyCanRead())
+                .required(true);
+        parser.addArgument("-d", "--dest-dir") //
+                .help("Top level directory of the new WebGoat lecacy project") //
+                .required(true)//
+                .type(Arguments.fileType().verifyIsDirectory().verifyCanWrite());
+        parser.addArgument("-l", "--lesson-name").help("Name of the lesson to be converted").required(true);
+        parser.addArgument("-o", "--overwrite").help("Overwrite an existing directory").action(Arguments.storeTrue());
+
+        try {
+            Options options = new Options();
+            parser.parseArgs(args, options);
+            new ConverterMain(options);
+        } catch (ArgumentParserException e) {
+            parser.handleError(e);
+            System.exit(1);
+        }
+    }
+}
---
+++ lesson-converter/src/main/java/org/owasp/webgoat/converter/PomCreator.java
@@ -0,0 +1,39 @@
+package org.owasp.webgoat.converter;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.FluentIterable;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class PomCreator {
+
+    public static final String ARTIFACT_ID = "${artifactId}";
+
+    public PomCreator() {
+    }
+
+    public List<String> createPom(final String artifactId) {
+        Preconditions.checkNotNull(artifactId, "artifactId cannot be null");
+
+        try {
+            URL pomUrl = getClass().getResource("/pom.example");
+            List<String> lines = Files.readAllLines(Paths.get(pomUrl.toURI()), Charsets.UTF_8);
+            return FluentIterable.from(lines).transform(new Function<String, String>() {
+                @Override
+                public String apply(String line) {
+                    return line.replace(ARTIFACT_ID, artifactId);
+                }
+            }).toList();
+        } catch (IOException | URISyntaxException e) {
+            throw new ConverterException("Unable to read pom.example, should be in the src/main/resources directory",
+                    e);
+        }
+    }
+}

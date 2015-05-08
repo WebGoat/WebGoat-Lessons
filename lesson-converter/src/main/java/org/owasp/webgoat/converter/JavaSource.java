b8d12d3fd460da1d4bab91263e84e10da8c64e70
Nanne Baars <nbaars@xebia.com>
2015-05-08 12:04:41 +0300

    Moved changing package to JavaSource Import for superclass is now automatically added WebGoatI18N replacement also moved to JavaSource

--- lesson-converter/src/main/java/org/owasp/webgoat/converter/JavaSource.java
+++ lesson-converter/src/main/java/org/owasp/webgoat/converter/JavaSource.java
@@ -1,11 +1,18 @@
 package org.owasp.webgoat.converter;
 
 
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -39,6 +46,68 @@ public class JavaSource {
         }
         Logger.end();
         return referencedProperties;
+    }
+
+    public String getSuperClass(List<String> lines) {
+        Optional<String> line = FluentIterable.from(lines).firstMatch(new Predicate<String>() {
+            @Override
+            public boolean apply(String line) {
+                return line.contains("extends");
+            }
+        });
+        if (line.isPresent()) {
+            return StringUtils.substringAfterLast(line.get(), "extends");
+        }
+        throw new ConverterException("Unable to detect super class for lesson " + javaSourceFile.getFileName());
+    }
+
+    private void rewritePackageName(List<String> lines) {
+        Logger.log("Rewriting package name...");
+        ListIterator<String> it = lines.listIterator();
+        while (it.hasNext()) {
+            String line = it.next();
+            if (line.contains("package org.owasp.webgoat.lessons;")) {
+                it.set(line.replace("package org.owasp.webgoat.lessons;", "package org.owasp.webgoat.plugin;"));
+            }
+        }
+    }
+
+    private void addImportForSuperclass(List<String> lines) {
+        String superClass = getSuperClass(lines);
+        String importToAdd = String.format("import org.owasp.webgoat.lessons.%s;", superClass.trim());
+        Logger.log("Adding import for package superclass %s", importToAdd);
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+            if (line.startsWith("import")) {
+                if (line.compareTo(importToAdd) >= 0) {
+                    lines.add(i, importToAdd);
+                    break;
+                }
+            }
+        }
+    }
+
+    public boolean containsReferenceToJavax() throws IOException {
+        List<String> lines = Files.readAllLines(javaSourceFile);
+        return false;
+    }
+
+    public List<String> getLines() throws IOException {
+        List<String> lines = Files.readAllLines(javaSourceFile);
+        rewritePackageName(lines);
+        addImportForSuperclass(lines);
+        replaceWebgoatI18N(lines);
+
+        return lines;
+    }
+
+    private void replaceWebgoatI18N(List<String> lines) {
+        Logger.log("Changing WebGoatI18N to LabelManager...");
+        ListIterator<String> it = lines.listIterator();
+        while (it.hasNext()) {
+            String line = it.next();
+            it.set(line.replace("WebGoatI18N.get", "getLabelManager().get"));
+        }
     }
 
     public Path getJavaSourceFile() {

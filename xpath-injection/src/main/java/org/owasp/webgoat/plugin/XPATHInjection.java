80d1d38b8ce2205da00252dc76a8549357a881fe
= <=>
2016-06-13 01:52:59 -0400

    Fix issue #231
    Fix the above issue - Lesson Ajax Security "LAB: Client Side Filtering"
    - can't validate stage 2 (#231). Forward slash was not being applied to
    the above function. Added forward slash so the page will not return a
    404.

--- xpath-injection/src/main/java/org/owasp/webgoat/plugin/XPATHInjection.java
+++ xpath-injection/src/main/java/org/owasp/webgoat/plugin/XPATHInjection.java
@@ -134,7 +134,7 @@ public class XPATHInjection extends LessonAdapter
                 return ec;
             }
 
-            String dir = LessonUtil.getLessonDirectory(s, this) + "/xml/" + "/EmployeesData.xml";
+            String dir = LessonUtil.getLessonDirectory(s, this) + "/xml/" + "EmployeesData.xml";
             File d = new File(dir);
             XPathFactory factory = XPathFactory.newInstance();
             XPath xPath = factory.newXPath();
